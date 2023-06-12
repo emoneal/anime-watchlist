416ec6af1e5e1092d99a1c89a857568497295a66
Emi ONeal <atashiemichandesu@gmail.com>
2023-06-12 17:12:36 -0400

    added production build folder and edited server config for deployment

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const path = require("path")
 
 // Middleware (to fire every request)
 app.use(express.json()) // Looks for request body
 app.use(morgan('dev')) // Logs all requests to server
 
 // Connect to DB
-mongoose.connect('mongodb://localhost:27017/animelistdb', {useNewUrlParser: true})
-    .then(() => console.log("Connected to DB"))
-    .catch(err => console.error(err))
+mongoose.connect(
+    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.k5floph.mongodb.net/`,
+    () => console.log('Connected to the DB')
+  )
 
 // Routes //
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use("/animes", require("./routes/animeListRouter"))
+
 
 // Error Handler
 app.use((err, req, res, next) => {
@@ -22,6 +26,10 @@ app.use((err, req, res, next) => {
 })
 
 // Server Listen //
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 
 app.listen(9000, () => {
     console.log("The server is running on Port 9000")
